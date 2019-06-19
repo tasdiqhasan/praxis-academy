1f53e6dd5c1545368b504f77dbe6277f63ca75ac
Tasdiq Hasan <hasantdq@gmail.com>
2019-06-19 13:58:31 +0700

    belajar variabel

---
+++ files/Variabel.java
@@ -0,0 +1,12 @@
+class Variabel {
+
+    public void outputApp() {
+        int angka = 15;
+        System.out.println(angka);
+    }
+
+    public static void main(String[] args) {
+        Variabel variabel = new Variabel();
+        variabel.outputApp();    
+    }
+}
