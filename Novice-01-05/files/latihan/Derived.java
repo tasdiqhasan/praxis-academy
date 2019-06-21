0927058f75892a2bc54497d4bac77627ad594c4a
Tasdiq Hasan <hasantdq@gmail.com>
2019-06-21 11:10:40 +0700

    contoh annotations pertama

---
+++ Novice-01-05/files/latihan/Derived.java
@@ -0,0 +1,20 @@
+class Base
+{
+     public void display()
+     {
+         System.out.println("Base display()");
+     }
+}
+class Derived extends Base
+{
+     @Override
+     public void display()
+     {
+         System.out.println("Derived display(int )");
+     }
+     public static void main(String args[])
+     {
+         Derived obj = new Derived();
+         obj.display();
+     }
+}
