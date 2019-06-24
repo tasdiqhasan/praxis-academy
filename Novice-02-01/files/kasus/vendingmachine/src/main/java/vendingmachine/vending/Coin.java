3661cfaa2b89056324228cc633c881c2f9687153
Tasdiq Hasan <hasantdq@gmail.com>
2019-06-24 15:13:09 +0700

    init kasus

---
+++ Novice-02-01/files/kasus/vendingmachine/src/main/java/vendingmachine/vending/Coin.java
@@ -0,0 +1,22 @@
+package vending; 
+/** 
+ * * Coins supported by Vending Machine. * @author Javin Paul
+ * @param  getDenomination( [description]
+ * @return                  [description]
+ */
+public enum Coin { 
+    PENNY(1), 
+    NICKLE(5), 
+    DIME(10), 
+    QUARTER(25); 
+    
+    private int denomination; 
+
+    private Coin(int denomination){ 
+        this.denomination = denomination; 
+    } 
+
+    public int getDenomination(){ 
+        return denomination; 
+    } 
+}
