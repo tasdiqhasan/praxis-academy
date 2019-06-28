584c49c0602c0d3d0c4aaee86c7ba44b83535557
Tasdiq Hasan <hasantdq@gmail.com>
2019-06-28 15:57:20 +0700

    tambah file kasus

---
+++ Novice-02-05/files/kasus/src/testing/BlockingThread.java
@@ -0,0 +1,37 @@
+package com.javageeks.concurrency.testing;
+
+public class BlockingThread extends Thread {
+    private SimpleBlockingQueue queue;
+    private boolean wasInterrupted = false;
+    private boolean reachedAfterGet = false;
+    private boolean throwableThrown;
+ 
+    public BlockingThread(SimpleBlockingQueue queue) {
+        this.queue = queue;
+    }
+     
+    public void run() {
+        try {
+            try {
+                queue.get();
+            } catch (InterruptedException e) {
+                wasInterrupted = true;
+            }
+            reachedAfterGet = true;
+        } catch (Throwable t) {
+            throwableThrown = true;
+        }
+    }
+ 
+    public boolean isWasInterrupted() {
+        return wasInterrupted;
+    }
+ 
+    public boolean isReachedAfterGet() {
+        return reachedAfterGet;
+    }
+     
+    public boolean isThrowableThrown() {
+        return throwableThrown;
+    }
+}
---
+++ Novice-02-05/files/kasus/src/testing/SimpleBlockingQueue.java
@@ -0,0 +1,30 @@
+package com.javageeks.concurrency.testing;
+
+public class SimpleBlockingQueue<T> {
+    private List<T> queue = new LinkedList<T>();
+ 
+    public int getSize() {
+        synchronized(queue) {
+            return queue.size();
+        }
+    }
+     
+    public void put(T obj) {
+        synchronized(queue) {
+            queue.add(obj);
+            queue.notify();
+        }
+    }
+     
+    public T get() throws InterruptedException  {
+        while(true) {
+            synchronized(queue) {
+                if(queue.isEmpty()) {
+                    queue.wait();
+                } else {
+                    return queue.remove(0);
+                }
+            }
+        }
+    }
+}
